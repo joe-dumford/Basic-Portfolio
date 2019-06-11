8784f103d553dd3f103c8330bb5096e236b7260e
Joe Dumford <dumford.joe@gmail.com>
2019-06-11 12:47:19 -0400

    social icons now have clickable functionality

--- assets/logic.js
+++ assets/logic.js
@@ -2,3 +2,11 @@ jQuery(document).ready(function ($) {
     $('.sidenav').sidenav();
    
 });
+
+/*
+$('btn').on('click', function(){
+    console.log('Anything at all');
+    window.location = 'http://www.google.com/';
+});
+*/
+
