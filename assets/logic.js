ddf32cbf888aba8aae9fe1bf24114673f631d8f9
Joe Dumford <dumford.joe@gmail.com>
2019-08-27 13:25:28 -0400

    Changed contact page layout and styling

--- assets/logic.js
+++ assets/logic.js
@@ -3,10 +3,4 @@ jQuery(document).ready(function ($) {
    
 });
 
-/*
-$('btn').on('click', function(){
-    console.log('Anything at all');
-    window.location = 'http://www.google.com/';
-});
-*/
 
